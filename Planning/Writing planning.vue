a7c9a09a6adb335207186e0366fe71d29c9f67ca
Apex619 <z3375125@ad.unsw.edu.au>
2020-05-25 16:10:03 +1000

    Updating with additional folder and files

---
+++ Planning/Writing planning.vue
@@ -0,0 +1,80 @@
+<!-- Tufts VUE 3.3.0 concept-map (New Map.vue) 2020-05-25 -->
+<!-- Tufts VUE: http://vue.tufts.edu/ -->
+<!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/C:/Program%20Files%20(x86)/VUE/VUE.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
+<!-- Do Not Remove: Saved date Mon May 25 11:56:44 EST 2020 by Hamza on platform Windows 8 6.2 in JVM 1.7.0_21-b11 -->
+<!-- Do Not Remove: Saving version @(#)VUE: built October 8 2015 at 1658 by tomadm on Linux 2.6.32-504.23.4.el6.x86_64 i386 JVM 1.7.0_21-b11(bits=32) -->
+<?xml version="1.0" encoding="US-ASCII"?>
+<LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="none" ID="0" label="New Map.vue"
+    created="1590371762056" x="0.0" y="0.0" width="1.4E-45"
+    height="1.4E-45" strokeWidth="0.0" autoSized="false">
+    <resource referenceCreated="1590371804926"
+        spec="C:\Users\Hamza\Documents\New Map.vue" type="1" xsi:type="URLResource">
+        <title>New Map.vue</title>
+        <property key="File" value="C:\Users\Hamza\Documents\New Map.vue"/>
+    </resource>
+    <fillColor>#FFFFFF</fillColor>
+    <strokeColor>#404040</strokeColor>
+    <textColor>#000000</textColor>
+    <font>SansSerif-plain-14</font>
+    <URIString>http://vue.tufts.edu/rdf/resource/498be70ec0a8017c0173be4a0e453ce7</URIString>
+    <layer ID="1" label="Layer 1" created="1590371762071" x="0.0"
+        y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+        <URIString>http://vue.tufts.edu/rdf/resource/498be70ec0a8017c0173be4a64da5d8d</URIString>
+    </layer>
+    <userZoom>1.0</userZoom>
+    <userOrigin x="-14.0" y="-14.0"/>
+    <presentationBackground>#202020</presentationBackground>
+    <PathwayList currentPathway="0" revealerIndex="-1">
+        <pathway ID="0" label="Untitled Pathway" created="1590371762056"
+            x="0.0" y="0.0" width="1.4E-45" height="1.4E-45"
+            strokeWidth="0.0" autoSized="false" currentIndex="-1" open="true">
+            <strokeColor>#B3993333</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/498be70ec0a8017c0173be4aa8dca531</URIString>
+            <masterSlide ID="2" created="1590371762087" x="0.0" y="0.0"
+                width="800.0" height="600.0" locked="true"
+                strokeWidth="0.0" autoSized="false">
+                <fillColor>#000000</fillColor>
+                <strokeColor>#404040</strokeColor>
+                <textColor>#000000</textColor>
+                <font>SansSerif-plain-14</font>
+                <URIString>http://vue.tufts.edu/rdf/resource/498be70ec0a8017c0173be4a0d7d01fc</URIString>
+                <titleStyle ID="3" label="Header"
+                    created="1590371762415" x="335.5" y="172.5"
+                    width="129.0" height="55.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-36</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/498be70ec0a8017c0173be4acb46e009</URIString>
+                    <shape xsi:type="rectangle"/>
+                </titleStyle>
+                <textStyle ID="4" label="Slide Text"
+                    created="1590371762415" x="346.5" y="281.5"
+                    width="107.0" height="37.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-22</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/498be70ec0a8017c0173be4a63b447a6</URIString>
+                    <shape xsi:type="rectangle"/>
+                </textStyle>
+                <linkStyle ID="5" label="Links" created="1590371762415"
+                    x="373.5" y="384.0" width="53.0" height="32.0"
+                    strokeWidth="0.0" autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#B3BFE3</textColor>
+                    <font>Gill Sans-plain-18</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/498be70ec0a8017c0173be4ae1f66f95</URIString>
+                    <shape xsi:type="rectangle"/>
+                </linkStyle>
+            </masterSlide>
+        </pathway>
+    </PathwayList>
+    <date>2020-05-25</date>
+    <modelVersion>6</modelVersion>
+    <saveLocation>C:\Users\Hamza\Documents</saveLocation>
+    <saveFile>C:\Users\Hamza\Documents\New Map.vue</saveFile>
+</LW-MAP>
