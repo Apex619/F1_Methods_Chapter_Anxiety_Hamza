3632cffbb984ffcf6a22157eb987763473cc37d5
Apex619 <z3375125@ad.unsw.edu.au>
2021-01-11 20:41:56 +1100

    Updates and example code for mixed models added

--- Scripts/EXAMPLE_MIXED MODEL.R
+++ Scripts/EXAMPLE_MIXED MODEL.R
@@ -1,41 +1,60 @@
 ### MIXED EFFECTS MODELLING
 
 # Mixed effects models are useful when working with multiple data points from the same subject. It works 
-#around the ANOVA assumption that all data points are independent. 
+#around the ANOVA assumption that all data points are independent. Further, it can work with datasets that
+# have missing data. 
 
-# Example Dataset: Runners
+# Example: Total distance travelled by zebrafish in anxiety tests
 
-# Here we will work with a very basic dataset which involves the times of runners running 100m sprints once
-# every week for 3 weeks. In addition, there is data indicating which school they attended. Each school 
-# hypothetically has different programs for helping athletes attain faster
-# sprint times. Now there are usually more variables that come into play for sprinting, 
-# however we will assume that all runners are uniform to some
-# degree (background, weight, age etc.) for simplicity sake.
+# We wanted to know if zebrafish travel more in short tanks or tall tanks, and whether this was impacted
+# by sex and water condition. Additionally, we want to know how consistent the zebrafish are in their 
+# results. 
 
-# Import Data
 
-running <- read.csv("./Data/runners_example.csv") 
-str(running)
-running$Week <- as.factor(running$Week) #Convert week to a factor
+# To answer these, we will do mixed effects modelling as well as repeatability analysis. 
 
-# Let's imagine we have 2 main questions:
+#Mixed effects modelling
 
-# (1) Which school is better at training athletes to have faster sprint times?
-# (2) How consistent are the runners in their sprint times? 
+library(lme4) #Package used to run mixed effects models
+library(lmerTest) #Package to display p-values of mixed effects model
+library(sjPlot) #Useful package in displaying results 
 
-# To answer these, we will use mixed effects modelling as well as repeatability analysis.
+# To build our mixed model:
 
-#Mixed effects model
+Model_Tot_Dist <- #assign our model an appropriate name
+  lmer(tot_dist ~ #use the function lmer and specify our response variable (in this case total distance)
+                         Tank + Sex + water_time + #specify our fixed factors (tank type, sex and water)
+                         (1 | Fish_ID), #specify our random effect (fish ID)
+                       data = Anxiety_Joined) #specify dataset
 
-library(lme4)
+hist(residuals(Model_Tot_Dist)) #histogram of residuals to check for normal distribution
 
-running_model <- lmer(Time ~ School + Week + (1 | Runner.ID), data = running)  
-tab_model(running_model)
-hist(residuals(running_model))
+#assumption of normality is satisfied, we can now examine the results of our model
+
+tab_model(Model_Tot_Dist) #displaying results in a nice table
+
+# Overall, from the table output, based on our intercept, zebrafish travelled significantly less in
+# tall tanks. Males also travelled less and as time went on and stress hormones were added, zebrafish
+# also swam less. However the latter two results are insignificant. 
+
+# To conduct repeatability analysis, we will use the package rptR. This package uses the mixed effects
+# model framework and requires specifying a mixed model similiar to what we just produced before.
+
+# Unadjusted repeatabilities will involve no fixed factors and only the random factor. However, we will
+# conduct adjusted analyses as water condition was a signifcant factor in our overall models (see paper)
 
 library(rptR)
 
-rpt_running <- rpt(Time ~ School + Week + (1 |  Runner.ID), grname = "Runner.ID", 
-         data = running, datatype = "Gaussian", nboot = 100, npermut = 100)
+total_distance_example <- #using the function rpt
+  rpt(tot_dist ~ #specifying our response variable
+                                water_time + #fixed factor
+                                (1 |  Fish_ID), #random effect
+                              grname = "Fish_ID", #random effect 
+                              data = Anxiety_Joined, #dataframe
+                              datatype = "Gaussian", nboot = 1000, npermut = 1000) #bootstraps and 
+#                                                                                   permutations to obtain 
+#                                                                                   SE and CI
 
-rpt_running
+total_distance_example #run to see repeatability
+
+# We obtain a repeatability of 0.26 which is moderate
